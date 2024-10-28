da60fc787e1a9a6d54ed0014ba74dff31de9285d
David Berlin <david@stackbit.com>
2024-10-28 08:41:17 +0200

    redirect tests

---
+++ app/test/page.jsx
@@ -0,0 +1,13 @@
+
+export const metadata = {
+    title: 'Test'
+};
+
+
+export default function Page() {
+    return (
+        <div className="flex flex-col gap-6 sm:gap-12">
+           a page
+        </div>
+    );
+}
